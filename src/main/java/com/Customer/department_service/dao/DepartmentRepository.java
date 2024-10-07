29812ebb16aaf62defe99880674f1f4d725fe6e3
manoejKr <287642@ust.com>
2024-10-07 14:02:27 +0530

    changes in repo class

--- src/main/java/com/Customer/department_service/dao/DepartmentRepository.java
+++ src/main/java/com/Customer/department_service/dao/DepartmentRepository.java
@@ -6,6 +6,8 @@ import com.Customer.department_service.Entity.DepartmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface DepartmentRepository extends JpaRepository< DepartmentEntity,Long> {
     
